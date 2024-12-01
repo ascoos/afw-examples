bc5e6f85942ac5c2e3b0a62379c19ad130f1efd9
Ascoos Projects <ascoos@users.noreply.github.com>
2024-12-01 05:39:12 +0200

    24.0.0

---
+++ autoload.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ ************************************************************************************
+ * @ASCOOS-NAME        	: ASCOOS CMS 24'                                            *
+ * @ASCOOS-VERSION     	: 24.0.0                                                    *
+ * @ASCOOS-CATEGORY    	: Framework (Frontend and Administrator Side)               *
+ * @ASCOOS-CREATOR     	: Drogidis Christos                                         *
+ * @ASCOOS-SITE        	: www.ascoos.com                                            *
+ * @ASCOOS-LICENSE     	: [Commercial] http://docs.ascoos.com/lics/ascoos/AGL.html  *
+ * @ASCOOS-COPYRIGHT   	: Copyright (c) 2007 - 2024, AlexSoft Software.             *
+ ************************************************************************************
+ *
+ * @package            	: ASCOOS FRAMEWORK (AFW)
+ * @subpackage         	: ASCOOS FRAMEWORK Autoload File for examples
+ * @source             	: [EXAMPLES]/autoload.php
+ * @fileNo             	: 
+ * @version            	: 24.0.2
+ * @created            	: 2024-11-30 20:00:00 UTC+3 
+ * @updated            	: 
+ * @author             	: Drogidis Christos
+ * @authorSite         	: www.alexsoft.gr
+ * @license 			: AGL-F
+ * 
+ * @since PHP 8.2.0
+ */
+declare(strict_types=1);
+require_once "[AFW PATH]/autoloader.php";
+
+?>
