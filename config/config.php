217b4c9314f9cc05099c813ae0cd8c8aedf93459
Ascoos Projects <ascoos@users.noreply.github.com>
2024-12-19 07:21:30 +0200

    24.0.7

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ return [
     'afw_examples_fonts' => '',
     'awes' => false,
     'version' => 2400070000,            
-    'afw_version' => 2400070000,            
-    'ascoos_cms_vrsion' => 2400000000,            
+    'afw_min_version' => 2400070000,            
+    'ascoos_cms_version' => 2400000000,            
 ];
 ?>
