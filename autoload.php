32a2f86ba7f72516070ffe0ae770d5d63488bb5d
Ascoos Projects <ascoos@users.noreply.github.com>
2024-12-19 07:20:54 +0200

    24.0.7

--- autoload.php
+++ autoload.php
@@ -18,21 +18,38 @@
  *
  * @package            	: ASCOOS FRAMEWORK (AFW)
  * @subpackage         	: ASCOOS FRAMEWORK Autoload File for examples
- * @source             	: [EXAMPLES]/autoload.php
+ * @source             	: afw-examples/autoload.php
  * @fileNo             	: 
- * @version            	: 24.0.4
+ * @version            	: 24.0.7
  * @created            	: 2024-11-30 20:00:00 UTC+3 
- * @updated            	: 2024-12-05 07:00:00 UTC+3
+ * @updated            	: 2024-12-18 07:00:00 UTC+3
  * @author             	: Drogidis Christos
  * @authorSite         	: www.alexsoft.gr
  * @license 			: AGL-F
  * 
  * @since PHP 8.2.0
  */
-//declare(strict_types=1);
-$afw_examples = str_replace('\\', '/', __DIR__);
-$afw_path = str_replace('/afw-examples', '', str_replace('\\', '/', __DIR__)).'/afw';
-$afw_examples_fonts = $afw_examples.'/fonts';
+declare(strict_types=1);
+
+use ASCOOS\FRAMEWORK\Kernel\Core\{
+    TObject,
+    TError
+};
+
+
+defined ("ALEXSOFT_RUN_CMS") OR define ("ALEXSOFT_RUN_CMS", True);
+defined('ASCOOS_RUN') OR define('ASCOOS_RUN', True);
+defined('ASCOOS_FRAMEWORK_RUN') OR define('ASCOOS_FRAMEWORK_RUN', True);
+
+$exConf = require_once "config/config.php";
+
+//
+$afw_examples = (!empty($exConf['afw_examples_path'])) ? $exConf['afw_examples_path'] : str_replace('/config', '', str_replace('\\', '/', __DIR__));
+//
+$afw_examples_fonts = (!empty($conf['afw_examples_fonts'])) ? $conf['afw_examples_fonts'] : $afw_examples.'/fonts';
+//
+$afw_path = (!empty($exConf['afw_path'])) ? $exConf['afw_path'] : str_replace('/afw-examples', '', str_replace('\\', '/', __DIR__)).'/afw';
+
 
 /**
  * for paths:
@@ -44,6 +61,23 @@ $afw_examples_fonts = $afw_examples.'/fonts';
  * accordingly so that it can work.
  * 
  */
-require_once($afw_path.'/autoload.php'); 
+ if (!$exConf['awes']) {
+    require_once $afw_path."/autoload.php"; 
+}
 
+try {
+    $obj = new TObject([
+        'version'=>$exConf['version']
+    ]);
+
+    if ($exConf['afw_min_version'] > $obj->getClassVersion() ) {
+        $e = new TError(sprintf("You have the <b>%s</b> version of Ascoos Framework. The minimum version required is <b>%s</b>.", intToVersionString($obj->getClassVersion(), 'XX.XX.XX'), intToVersionString($exConf['afw_min_version'], 'XX.XX.XX')), E_ASCOOS_NOTICE_INVALID_VERSION);
+        echo $e;
+        $e->Free($e);
+        exit;
+    }
+    $obj->Free($obj);
+} catch (TError $e) {
+    echo $e->getMessage();
+}
 ?>
